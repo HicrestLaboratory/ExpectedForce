d7502b14aa5e4c929fee5b83b717a2abd79213af
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-02-18 12:50:08 +0100

    Update converter.py

--- converter.py
+++ converter.py
@@ -7,8 +7,26 @@ Created on Mon Dec 21 15:16:01 2020
 
 
 Graph Converter
-
 """
+import argparse
+import os
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Converts graph to be run with Expected Force")
+    
+    parser.add_argument("--input-file", default="fb_full.txt",
+        help="file that contains the already run experiments")
+    parser.add_argument("--delimiter", default=" ",
+        help="file that contains the already run experiments")
+
+    args = parser.parse_args()
+
+    input_file = args.input_file;
+    delimiter = args.delimiter;
+
 filename = "fb_full.txt"
 outfile = filename.split(".")[0] + "mapping.txt"
 outfile = filename.split(".")[0] + "converted.txt"
