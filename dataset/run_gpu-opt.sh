0abb6aae0d93b446a63e4518c16d97010f645168
Vella <flavio.vella@\unitn.it>
2023-01-27 09:50:39 +0100

    add optimization to dbg; make opt

---
+++ dataset/run_gpu-opt.sh
@@ -0,0 +1,25 @@
+
+#!/bin/sh
+graph=$1
+
+#echo 
+#./expected_force_serial ${graph} 1 out.txt
+echo "Run GPU"
+for b in 64 128 256 512
+do
+	for th in 256 512 1024
+	do
+		for sc in 1 2 4
+		do
+			echo =====================
+			echo `date`
+			echo bs = ${b} threads = ${th} streams = ${sc}
+			./expected_force_opt ${graph} ${b} ${th} ${sc} 1
+			echo =====================
+			echo ""
+			echo ""
+		done
+	done
+done
+
+
